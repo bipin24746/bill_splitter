3f4f73ea5d03119fcf2924319deba1c3c96d2cff
bipin24746 <sainjubipin247460@gmail.com>
2025-07-30 22:06:08 +0545

    starting the project bill splitter

---
+++ server/src/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+app.use(express.json);
+const port = 8080;
+
+app.listen(port,()=>{
+    console.log(`Backend Server starts to run on port ${port}`);
+})
